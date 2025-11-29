14a345f285c0d7b7c115604f6084daf129e448b7
Oliver Forral <intrepion@gmail.com>
2025-11-28 22:00:16 -0800

    Step 4: Set Up the Database Engine

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Change to your DB
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+# Create tables manually if needed
+# Base.metadata.create_all(bind=engine)

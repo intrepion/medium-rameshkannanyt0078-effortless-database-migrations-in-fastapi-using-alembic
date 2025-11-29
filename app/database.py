db11b3216e778c5ebcaecf466bd8895e407eba3a
Oliver Forral <intrepion@gmail.com>
2025-11-28 22:04:45 -0800

    black .

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from .models import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Change to your DB
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(bind=engine)
 # Create tables manually if needed
 # Base.metadata.create_all(bind=engine)
